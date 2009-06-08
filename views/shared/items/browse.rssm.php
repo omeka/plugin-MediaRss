ebaf2948f6f67d508ee34b0d560cb47be82267f3
Jeremy Boggs <jeremy@clioweb.org>
2009-06-08 01:46:48 +0000

    Fixes RSSM output to include all files for items that have thumbnails, fixes link to item in the feed, uses the items output uri helper to correctly generate the path to the RSSM feed.

--- views/shared/items/browse.rssm.php
+++ views/shared/items/browse.rssm.php
@@ -5,15 +5,18 @@
          <?php
             while($item = loop_items()):
             if (item_has_files()): 
+			while(loop_files_for_item($item)):
+		        $file = get_current_file();
+		        if ($file->hasThumbnail()):
          ?>
          <item>
              <title><?php echo item('Dublin Core', 'Title'); ?></title>
              <media:description><?php echo item('Dublin Core', 'Description'); ?></media:description>
-             <link><?php echo link_to_item(); ?></link>
-             <media:thumbnail url="<?php echo $item->Files[0]->getWebPath('archive'); ?>"/>
-             <media:content url="<?php echo $item->Files[0]->getWebPath('archive'); ?>"/>
+             <link><?php echo item_uri(); ?></link>
+             <media:thumbnail url="<?php echo $file->getWebPath('thumbnail'); ?>"/>
+             <media:content url="<?php echo $file->getWebPath('fullsize'); ?>"/>
          </item>
-            <?php endif; ?>
-        <?php endwhile; ?>
+            <?php endif; endwhile;?>
+        <?php endif; endwhile; ?>
      </channel>
 </rss>
