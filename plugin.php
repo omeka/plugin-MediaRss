53f080e7d2a41b21be7a0e5f49fb52d260ee88d3
Jeremy Boggs <jeremy@clioweb.org>
2011-03-02 02:47:20 +0000

    Adds hook and function to automatically output the Media RSS feed in public themes.

--- plugin.php
+++ plugin.php
@@ -3,6 +3,7 @@ define('MEDIARSS_PLUGIN_VERSION', get_plugin_ini('MediaRss', 'version'));
 
 add_filter('define_response_contexts', 'media_rss_response_context');
 add_filter('define_action_contexts', 'media_rss_action_context');
+add_plugin_hook('public_theme_header', 'media_rss_public_theme_header');
 
 function media_rss_action_context($context, $controller)
 {
@@ -28,4 +29,12 @@ function media_rss_rssm()
 	if (($request->getControllerName() == 'items' && $request->getActionName() == 'browse') || ($request->getControllerName() == 'index' && $request->getActionName() == 'index')) {
 	    return '<link rel="alternate" type="application/rss+xml" title="Media RSS Feed" href="'.items_output_uri('rssm').'" id="gallery"/>' . "\n";
 	}
+}
+
+/**
+ * Echos the Media RSS link, for the 'public_theme_header' hook.
+ */
+function media_rss_theme_header()
+{
+    echo media_rss_rssm();
 }
